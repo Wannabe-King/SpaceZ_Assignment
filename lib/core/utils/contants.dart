8aeeb4bd3ec4f32c041ea1704c511668cab1e77d
Priyanshu <sidarsunita2@gmail.com>
2025-11-22 02:00:22 +0530

    const color added

--- lib/core/utils/contants.dart
+++ lib/core/utils/contants.dart
@@ -4,11 +4,12 @@ import 'package:flutter/material.dart';
 
 const primaryColor = Color(0xFFC16B3E);
 const secondaryColor = Color(0xFFFDF9F7);
-
+const bannerColor = Color(0xFF316836);
 
 // Text Color
 
-const titleColor= Color(0xFF4B4E4B);
-const subtitleColor= Color(0xFF7D817D);
-const tagColor= Color(0xFF874B2C);
-const whileTextColor= Color(0xFFFFFFFF);
+const titleColor = Color(0xFF4B4E4B);
+const subtitleColor = Color(0xFF7D817D);
+const tagColor = Color(0xFF874B2C);
+const priceTextColor = Color(0xFFB82B14);
+const whileTextColor = Color(0xFFFFFFFF);
