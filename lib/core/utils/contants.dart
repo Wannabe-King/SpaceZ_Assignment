c76078336fc6570a8ec31094f9114623933dfcce
Priyanshu <sidarsunita2@gmail.com>
2025-11-22 01:55:15 +0530

    first commit

---
+++ lib/core/utils/contants.dart
@@ -0,0 +1,14 @@
+//Colors
+
+import 'package:flutter/material.dart';
+
+const primaryColor = Color(0xFFC16B3E);
+const secondaryColor = Color(0xFFFDF9F7);
+
+
+// Text Color
+
+const titleColor= Color(0xFF4B4E4B);
+const subtitleColor= Color(0xFF7D817D);
+const tagColor= Color(0xFF874B2C);
+const whileTextColor= Color(0xFFFFFFFF);
